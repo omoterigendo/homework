abafc5222802f5c1a7445b3c0d034831ec311e99
omoterigendo <75212867+omoterigendo@users.noreply.github.com>
2024-02-14 17:11:39 +0300

    add valentainday

---
+++ valentainday/script.js
@@ -0,0 +1,25 @@
+const buttonYes = document.querySelector('.yes');
+const buttonNo = document.querySelector('.no');
+
+let buttonYesFontSize = 2;
+let buttonNoFontSize = 2;
+
+buttonNo.addEventListener('click', function () {
+    doYesMore();
+});
+
+buttonYes.addEventListener('click', function () {
+    buttonYesPushed();
+});
+
+function doYesMore () {
+    buttonYesFontSize += .4;
+    buttonYes.style.fontSize = buttonYesFontSize + 'em';
+
+    buttonNoFontSize -= .2;
+    buttonNo.style.fontSize = buttonNoFontSize + 'em';
+}
+
+function buttonYesPushed (){
+    location.href = './page2.html';
+}
