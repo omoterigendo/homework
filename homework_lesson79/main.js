a44ba400558e7ae21accb0581bcea2e3804b9e72
omoterigendo <75212867+omoterigendo@users.noreply.github.com>
2024-04-12 16:29:31 +0300

    Add files via upload

---
+++ homework_lesson79/main.js
@@ -0,0 +1,9 @@
+let age = 18
+
+if(age >= 18) {
+  console.log('вы совершеннолетний');
+} else(console.log('вам еще нет 18'))
+
+
+age = 17
+age >= 18 ? console.log('вы совершеннолетний') : console.log('вам нет 18'); 
